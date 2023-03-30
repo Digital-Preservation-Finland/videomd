ff46b481703f8d182b076aade70bc1b29f41d130
Juha Lehtonen <juha.lehtonen@csc.fi>
2023-03-30 08:00:44 +0300

    KDKPAS-2987 Bump version and remove Python 2 support officially.

---
+++ RELEASE_NOTES.rst
@@ -0,0 +1,7 @@
+Release notes
+=============
+
+Version 0.8
+-----------
+
+- Python 2.7 support officially removed.
--- videomd/__init__.py
+++ videomd/__init__.py
@@ -1,2 +1,7 @@
-# flake8: noqa
-from .videomd import *
+"""Import basic library files for making convenient use of the library
+possible.
+"""
+__version__ = '0.8'
+
+# flake8 doesn't like these imports, but they are needed for other repos
+from .videomd import *  # noqa: F401,F403
